b4a24b0995f45f08253a8402628c4652ec7d4e29
Deutsch Gábor <deutschgabor92@gmail.com>
2020-11-19 18:00:01 +0100

    változtatások eszközölése

---
+++ week-01/day-04/seconds-in-a-day.ts
@@ -0,0 +1,10 @@
+'use strict'
+
+function clock() {
+let date = new Date();
+
+let hours: number = date.getHours();
+let minutes: number = date.getMinutes();
+let seconds: number = date.getSeconds();
+
+}
--- week-01/day-04/swap.ts
+++ week-01/day-04/swap.ts
@@ -6,4 +6,6 @@ let numberToSwap2: number = 526;
 [numberToSwap1, numberToSwap2] = [numberToSwap2, numberToSwap1]
 
 console.log(numberToSwap1);
-console.log(numberToSwap2);
+console.log(numberToSwap2)
+
+
