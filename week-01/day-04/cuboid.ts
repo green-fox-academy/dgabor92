a89eb825d76da546b75c957c18a56cb03fea3d53
Deutsch Gábor <deutschgabor92@gmail.com>
2020-11-19 21:37:25 +0100

    cuboid feladat kezdete

---
+++ week-01/day-04/cuboid.ts
@@ -0,0 +1,18 @@
+'use strict';
+
+// Write a program that stores 3 sides of a cuboid as variables (floats)
+// The program should write the surface area and volume of the cuboid like:
+//
+// Surface Area: 600
+// Volume: 1000
+
+let volume: number = 1000;
+
+let surfaceArea: number = 600;
+
+// 600 = a + b + c + d
+
+let a: number;
+let b: number;
+let c: number;
+let d: number;
--- week-01/day-04/seconds-in-a-day.ts
+++ week-01/day-04/seconds-in-a-day.ts
@@ -1,10 +1,9 @@
-'use strict'
+'use strict';
 
 function clock() {
-let date = new Date();
+  let date = new Date();
 
-let hours: number = date.getHours();
-let minutes: number = date.getMinutes();
-let seconds: number = date.getSeconds();
-
+  let hours: number = date.getHours();
+  let minutes: number = date.getMinutes();
+  let seconds: number = date.getSeconds();
 }
