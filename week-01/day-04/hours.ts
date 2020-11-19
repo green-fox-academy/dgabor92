d50606548c60bff1b3474f914c869d79d616c224
Deutsch Gábor <deutschgabor92@gmail.com>
2020-11-19 14:32:32 +0100

    hours.ts majdnem kész még az utolsó részlet finomításra szorul

---
+++ week-01/day-04/hours.ts
@@ -0,0 +1,22 @@
+"use strict";
+
+// An average Green Fox attendee codes 6 hours daily
+// The semester is 17 weeks long
+//
+// Print how many hours is spent with coding in a semester by an attendee,
+// if the attendee only codes on workdays.
+//
+// Print the percentage of the coding hours in the semester if the average
+// work hours weekly is 52
+
+let number1: number = 6;
+let number2: number = 17;
+
+// 6 hours per day in a week
+let week: number = 30;
+
+console.log(week * number2);
+
+let percentage: number = 52 * 17;
+
+console.log(percentage);
