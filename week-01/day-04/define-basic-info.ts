db8a543da9d75953c22ab1a7ad70c6fe048e1cc5
Deutsch Gábor <deutschgabor92@gmail.com>
2020-11-19 15:13:44 +0100

    define-basic-info.ts feladat kész!

---
+++ week-01/day-04/define-basic-info.ts
@@ -0,0 +1,13 @@
+
+'use strict';
+
+// Define several things as a variable, then print their values
+// Your name as a string
+// Your age as a number
+// Your height in meters as a number
+// Whether you are married or not as a boolean
+
+let name: string = 'Deutsch Gábor';
+let age: number = 28;
+let height: number = 170;
+let areYouMarried: boolean = false;
