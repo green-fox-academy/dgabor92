7fc5d4db29cad1ba6e9f68b4658c546a9b9d7b5c
Deutsch Gábor <deutschgabor92@gmail.com>
2020-11-19 17:15:46 +0100

    variable-mutation.ts feladat majdnem kész, a vége hiányzik

---
+++ week-01/day-04/variable-mutation.ts
@@ -0,0 +1,54 @@
+'use strict';
+
+let a: number = 3;
+// make it bigger by 10
+a = a + 10;
+
+console.log(a);
+
+let b: number = 100;
+// make it smaller by 7
+b = b - 7;
+
+console.log(b);
+
+let c: number = 44;
+// double c's value
+c = c * c;
+console.log(c);
+
+let d: number = 125;
+// divide d's value by 5
+d /= 5;
+console.log(d);
+
+let e: number = 8;
+// what's the cube of e's value?
+
+console.log(e);
+
+let f1: number = 123;
+let f2: number = 345;
+// tell if f1 is bigger than f2 (as a boolean)
+let f3: boolean = f1 > f2;
+
+console.log(f3);
+
+let g1: number = 350;
+let g2: number = 200;
+// tell if the double of g2 is bigger than g1 (pras a boolean)
+let g3: number = g2 * g2;
+
+let g4: boolean = g3 > g1;
+
+console.log(g4);
+
+let h: number = 1357988018575474;
+// tell if 11 is a divisor of h (as a boolean)
+
+let i1: number = 10;
+let i2: number = 3;
+// tell if i1 is higher than i2 squared and smaller than i2 cubed (as a boolean)
+
+let j: number = 1521;
+// tell if j is divisible by 3 or 5 (as a boolean)
