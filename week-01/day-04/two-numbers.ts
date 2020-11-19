cb42e61935b1fb308aeab388de889397cb3553e1
Deutsch Gábor <deutschgabor92@gmail.com>
2020-11-19 14:23:09 +0100

    two-numbers feladat kész!

---
+++ week-01/day-04/two-numbers.ts
@@ -0,0 +1,11 @@
+"use strict";
+
+let number1: number = 13;
+let number2: number = 22;
+
+for (let i = 0; i < 1; i++) {
+  console.log(number1 + number2);
+  console.log(number1 - number2);
+  console.log(number1 * number2);
+  console.log(number2 / number1);
+}
