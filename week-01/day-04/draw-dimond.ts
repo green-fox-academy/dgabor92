b88e9e48d153e7bf0d25c82f97c3aadd04cc35c6
Deutsch Gábor <deutschgabor92@gmail.com>
2020-11-20 10:37:00 +0100

    draw diamond feladat kész!

---
+++ week-01/day-04/draw-dimond.ts
@@ -0,0 +1,29 @@
+'use strict';
+
+let lineCount: number = 7;
+
+// Write a program that draws a
+// diamond like this:
+//
+//    *
+//   ***
+//  *****
+// *******
+//  *****
+//   ***
+//    *
+//
+// The diamond should have as many lines as lineCount is
+function diamond(val) {
+  var y,
+    w,
+    shape = '';
+
+  for (y = 0; y < val * 2 - 1; y++) {
+    w = y < val ? y : val * 2 - y - 2;
+    shape += Array(val - w).join(' ') + Array(w + 1).join('* ') + '*\n';
+  }
+  console.log(shape);
+}
+
+diamond(4);
