a85cae8f383048c75f122b190486a83f59106788
Deutsch Gábor <deutschgabor92@gmail.com>
2020-11-23 15:12:50 +0100

    change-elements.ts feladat kész!

---
+++ week-02/day-01/change-elements.ts
@@ -0,0 +1,19 @@
+'use strict';
+
+// -  Create an array named `numbers` with the following content: `[1, 2, 3, 8, 5, 6]`
+// -  Change the 8 to 4 with the `.map` method
+// -  Print the fourth element as a test
+
+let numbers: number[] = [1, 2, 3, 8, 5, 6];
+
+console.log(numbers);
+
+function change(): number[] {
+  numbers.splice(3, 0);
+  numbers[3] = 4;
+  return;
+}
+
+change();
+
+console.log(numbers[3]);
