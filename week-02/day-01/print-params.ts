be58eb02014ab61e682e256d577fce4afed7994e
Deutsch Gábor <deutschgabor92@gmail.com>
2020-11-23 20:25:02 +0100

    print-params.ts feladat kész

---
+++ week-02/day-01/print-params.ts
@@ -0,0 +1,9 @@
+// -  Create a function called `printParams`
+//    which logs to the console the input parameters
+//    (can have multiple number of arguments)
+
+function printParams(...input: any): void {
+  console.log(...input);
+}
+
+printParams('hello', 'words');
