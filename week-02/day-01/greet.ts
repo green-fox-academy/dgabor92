f4c021362bbcfcf0a69fd5c4d4c998077b90d3c1
Deutsch Gábor <deutschgabor92@gmail.com>
2020-11-23 17:21:30 +0100

    greet.ts feladat kész!

---
+++ week-02/day-01/greet.ts
@@ -0,0 +1,13 @@
+// -  Create variable named `nameToGreet` and assign the value `Green Fox` to it
+// -  Create a function called `greet` that greets it's input parameter
+//     -  Greeting is printing e.g. `Greetings, dear Green Fox`
+// -  Greet `nameToGreet`
+
+let nameToGreet: string = 'Green Fox';
+
+function greet(greet: string): void {
+  console.log('Greetings, dear ' + greet);
+}
+
+greet(nameToGreet);
+// greet('szia');
