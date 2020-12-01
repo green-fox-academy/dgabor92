d06713ac33d5db0bdb2e11751ac8bafea48d2fba
Deutsch Gábor <deutschgabor92@gmail.com>
2020-12-01 15:00:45 +0100

    divide-by-zero.ts feladat kész!

---
+++ week-03/day-02/divide-by-zero.ts
@@ -0,0 +1,20 @@
+// Create a function that takes a number
+// divides ten with it,
+// and prints the result.
+// It should print 'fail' if the parameter is 0
+
+'use strict';
+
+function divideNumber(arr: number): number {
+  try {
+    if (arr === 0) {
+      throw 'ERROR';
+    } else {
+      return arr / 10;
+    }
+  } catch (err) {
+    console.log('Fail');
+  }
+}
+
+console.log(divideNumber(0));
