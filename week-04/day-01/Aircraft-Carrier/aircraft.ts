1df949741f45802cb3dde97745fc4abeaa3534e3
Deutsch Gábor <deutschgabor92@gmail.com>
2020-12-07 17:26:20 +0100

    method részmegoldás

--- week-04/day-01/Aircraft-Carrier/aircraft.ts
+++ week-04/day-01/Aircraft-Carrier/aircraft.ts
@@ -12,7 +12,28 @@ export default class Aircraft {
     this._baseDamage = this._baseDamage * this._maxAmmo;
     this._maxAmmo = 0;
   }
-  refill(number: number) {}
+  refill(num: number) {
+    if (num >= 8) {
+      F16._maxAmmo = 8;
+    } else {
+      console.log(`Remaining ammo: ${F16._maxAmmo - num}`);
+    }
+    if (num >= 12) {
+      F35._maxAmmo = 12;
+    } else {
+      console.log(`Remaining ammo: ${F35._maxAmmo - num}`);
+    }
+  }
+  getType() {
+    return typeof Aircraft;
+  }
+  getStatus() {
+    console.log(
+      `Type: ${Aircraft.arguments} Ammo:${this._maxAmmo}, Base Damage: ${
+        this._baseDamage
+      }, All Damage:${this._baseDamage * this._maxAmmo}`
+    );
+  }
 }
 
 let F16 = new Aircraft(8, 30);
@@ -24,3 +45,10 @@ F16.fight();
 console.log(F16);
 F35.fight();
 console.log(F35);
+
+F16.refill(20);
+F35.refill(50);
+console.log(F16);
+console.log(F35);
+
+F16.getStatus();
