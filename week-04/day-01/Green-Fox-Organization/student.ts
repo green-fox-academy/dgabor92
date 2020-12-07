9646e7b7cbb387fe931a0628f60743fbf181020a
Deutsch Gábor <deutschgabor92@gmail.com>
2020-12-07 13:28:39 +0100

    student rész kész!

---
+++ week-04/day-01/Green-Fox-Organization/student.ts
@@ -0,0 +1,27 @@
+'use strict';
+
+import Person from './person';
+export default class Student extends Person {
+  private _previousOrganization: string;
+  private _skippedDay: number;
+
+  constructor(
+    previousOrganization: string,
+    name?: string,
+    age?: number,
+    gender?: string
+  ) {
+    super(name, age, gender);
+    this._previousOrganization = previousOrganization = 'The School of Life';
+    this._skippedDay = 0;
+  }
+  getGoal(): string {
+    return `My goal is: Be a junior software developer`;
+  }
+  introduce(): string {
+    return `Hi, I'm ${this._name}, a ${this._age} year old ${this._gender} from ${this._previousOrganization} who skipped ${this._skippedDay} days from the course already.`;
+  }
+  skippedDay(numberOfDays: number) {
+    this._skippedDay += numberOfDays;
+  }
+}
