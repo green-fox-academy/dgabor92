4982448534645ddf5f67d01f13c7d4c90e8e59ce
Deutsch Gábor <deutschgabor92@gmail.com>
2020-12-07 14:25:16 +0100

    cohort feladatrész kész!

---
+++ week-04/day-01/Green-Fox-Organization/cohort.ts
@@ -0,0 +1,24 @@
+'use strict';
+import Student from './student';
+import Mentor from './mentor';
+export default class Cohort {
+  private _name: string;
+  private _students: Student[];
+  private _mentors: Mentor[];
+
+  constructor(name: string) {
+    this._name = name;
+    this._students = [];
+    this._mentors = [];
+  }
+
+  addStudent(Student) {
+    this._students.push(Student);
+  }
+  addMentor(Mentor) {
+    this._mentors.push(Mentor);
+  }
+  info() {
+    console.log(Cohort, this._students.length, this._mentors.length);
+  }
+}
