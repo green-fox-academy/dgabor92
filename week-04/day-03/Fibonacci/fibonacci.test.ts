df4858c1c2d07873131de5d894d2d8c9a9d2cde8
Deutsch Gábor <deutschgabor92@gmail.com>
2020-12-09 16:44:47 +0100

    Fibonacci változtatásai

--- week-04/day-03/Fibonacci/fibonacci.test.ts
+++ week-04/day-03/Fibonacci/fibonacci.test.ts
@@ -7,3 +7,31 @@ test('should be return an array ', (t) => {
   t.equal(actual, expected);
   t.end();
 });
+
+test('should be return  [0,1] with negative number', (t) => {
+  let actual = listFibonacci(-200);
+  let expected = [0, 1];
+  t.equal(actual.toString(), expected.toString());
+  t.end();
+});
+
+test('should be return  [0,1] with null number', (t) => {
+  let actual = listFibonacci(null);
+  let expected = [0, 1];
+  t.equal(actual.toString(), expected.toString());
+  t.end();
+});
+
+test('should be return  [0,1] with undefined number', (t) => {
+  let actual = listFibonacci(undefined);
+  let expected = [0, 1];
+  t.equal(actual.toString(), expected.toString());
+  t.end();
+});
+
+test('should be return  [0,1] with NaN number', (t) => {
+  let actual = listFibonacci(NaN);
+  let expected = [0, 1];
+  t.equal(actual.toString(), expected.toString());
+  t.end();
+});
--- week-04/day-03/Fibonacci/fibonacci.ts
+++ week-04/day-03/Fibonacci/fibonacci.ts
@@ -1,11 +1,10 @@
 'use strict';
 
-let fibonacci: number[] = [0, 1];
-
 export default function listFibonacci(num: number) {
+  let fibonacci: number[] = [0, 1];
   for (let i: number = 2; i < num; i++) {
     fibonacci[i] = fibonacci[i - 2] + fibonacci[i - 1];
   }
   return fibonacci;
 }
-// console.log(listFibonacci(10));
+console.log(listFibonacci(10));
