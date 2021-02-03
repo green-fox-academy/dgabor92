f293647fd4f8fd8d2c8bb5243053e8a5a713b437
Deutsch Gábor <deutschgabor92@gmail.com>
2021-02-03 16:22:11 +0100

    database created

--- week-10/foxplayer/server.js
+++ week-10/foxplayer/server.js
@@ -7,6 +7,22 @@ const PORT = 3000;
 app.use(express.static('public')); // use static html file
 app.use(express.json()); //bodyparse
 
+const conn = mysql.createConnection({
+  // create connection with database
+  host: 'localhost',
+  user: 'root',
+  password: 'password',
+  database: 'musicplayer',
+  insecureAuth: 'true',
+});
+
+conn.connect((err) => {
+  if (err) {
+    console.log(err.toString());
+  }
+  console.log('Connected to mysql');
+});
+
 app.listen(PORT, () => {
   console.log('Connected to the server');
 });
